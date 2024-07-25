a2f7d02928bfc99a6449134a578e59828b6d032c
MahdiHaddad <mahdihaddad82@gmail.com>
2024-07-25 21:00:45 +0330

    complete footer

--- src/components/ProgressItem.jsx
+++ src/components/ProgressItem.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import RevealUp from "../components/animation/RevealUp";
 
 export default function ProgressItem({ item }) {
   return (
-    <div className="relative flex flex-col justify-start items-center 2xl:mb-3">
-      <div className="absolute text-[20px] sm:text-[30px] md:text-[20px] 2xl:text-[25px] -top-6 md:-top-4 lg:-top-4   rounded-full flex justify-center shadow-lg items-center z-10 w-12 h-12 sm:w-16 sm:h-16 md:w-9 md:h-9 lg:w-12 lg:h-12     bg-[#A0C49D] bg-opacity-95">
-        {item.number}
+    <RevealUp>
+      <div className="relative flex flex-col justify-start items-center 2xl:mb-3">
+        <div className="absolute text-[20px] sm:text-[30px] md:text-[20px] 2xl:text-[25px] -top-6 md:-top-4 lg:-top-4   rounded-full flex justify-center shadow-lg items-center z-10 w-12 h-12 sm:w-16 sm:h-16 md:w-9 md:h-9 lg:w-12 lg:h-12     bg-[#A0C49D] bg-opacity-95">
+          {item.number}
+        </div>
+        <div className="text-[24px] sm:text-[30px] md:text-[18px] lg:text-[24px]  mt-2 flex justify-center items-center shadow-lg w-36 h-36 md:w-[120px] md:h-[120px] sm:w-48 sm:h-48 lg:w-36 lg:h-36 p-6 rounded-full  text-center bg-[#E1ECC8] bg-opacity-50">
+          {item.text}
+        </div>
       </div>
-      <div className="text-[24px] sm:text-[30px] md:text-[18px] lg:text-[24px]  mt-2 flex justify-center items-center shadow-lg w-36 h-36 md:w-[120px] md:h-[120px] sm:w-48 sm:h-48 lg:w-36 lg:h-36 p-6 rounded-full  text-center bg-[#E1ECC8] bg-opacity-50">
-        {item.text}
-      </div>
-    </div>
+    </RevealUp>
   );
 }
--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { usePost } from "../context/PostProvider";
+import RevealRtl from "../components/animation/RevealRtl";
+import RevealLtr from "../components/animation/RevealLtr";
+
 import { FaInstagram } from "react-icons/fa6";
 import { BsTelegram } from "react-icons/bs";
 import { IoCallOutline } from "react-icons/io5";
@@ -19,40 +22,44 @@ export default function Footer() {
   return (
     <div
       ref={info}
-      className="bg-[#A0C49D] border-2 m-3 rounded-xl p-4  flex  flex-row-reverse justify-around items-center"
+      className="bg-[#A0C49D] border-2 m-3 rounded-xl sm:p-5 flex sm:flex-row-reverse 2s:flex-col justify-around items-center 2s:gap-14 sm:gap-0"
     >
-      <div className="flex  sm:flex-col items-end gap-1">
-        <h1 className="mb-2 text-[50px]">ارتباط با ما</h1>
-        <div className="flex gap-1 flex-row-reverse text-[25px]">
-          <IoCallOutline />
-          <p>: تماس با ما</p>
-          <p>0996 281 2008</p>
-        </div>
-        <div className="flex gap-2 flex-row-reverse text-[25px]">
-          <CiLocationOn />
-          <p>: نشانی</p>
-          <p>قزوین، مجتمج تجاری البرز</p>
-        </div>
-        <div className="flex flex-row-reverse justify-start gap-7 w-full mt-3 text-[25px]">
-          <button onClick={openInstagram}>
-            <FaInstagram />
-          </button>
-          <button onClick={openTelegram}>
-            <BsTelegram />
-          </button>
-          {/* <button>
+      <RevealRtl>
+        <div className="flex flex-col items-end gap-1">
+          <h1 className="mb-2 text-[50px]">ارتباط با ما</h1>
+          <div className="flex gap-1 flex-row-reverse sm:text-[25px]">
+            <IoCallOutline />
+            <p>: تماس با ما</p>
+            <p>0996 281 2008</p>
+          </div>
+          <div className="flex gap-2 flex-row-reverse text-[25px]">
+            <CiLocationOn />
+            <p>: نشانی</p>
+            <p>قزوین، مجتمج تجاری البرز</p>
+          </div>
+          <div className="flex flex-row-reverse justify-start gap-7 w-full mt-3 text-[25px]">
+            <button onClick={openInstagram}>
+              <FaInstagram />
+            </button>
+            <button onClick={openTelegram}>
+              <BsTelegram />
+            </button>
+            {/* <button>
             <img alt="لوگوی ایتا" src="../../public/images/eitaa-icon-2005x2048-z22hivx5.png" className="w-6 h-6"/>
           </button> */}
+          </div>
         </div>
-      </div>
-      <div>
-        <img
-          className="drop-shadow-icon"
-          src="../../public/images/neonwich.png"
-          width={150}
-          height={100}
-        />
-      </div>
+      </RevealRtl>
+      <RevealLtr>
+        <div>
+          <img
+            className="drop-shadow-icon"
+            src="../../public/images/neonwich.png"
+            width={150}
+            height={100}
+          />
+        </div>
+      </RevealLtr>
     </div>
   );
 }
